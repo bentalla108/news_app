c0512316814ca462d9906d1784589d4bcd21c424
bentalla108 <tallambaye108@gmail.com>
2023-10-14 16:15:03 +0200

    Local Storage

---
+++ lib/features/daily_news/domain/entities/articles_entities.dart
@@ -0,0 +1,19 @@
+import 'package:equatable/equatable.dart';
+
+class Article_Entities extends Equatable {
+  final int? id;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publishedAt;
+  final String? content;
+
+  Article_Entities(this.id, this.author, this.title, this.description, this.url,
+      this.urlToImage, this.publishedAt, this.content);
+
+  @override
+  List<Object?> get props =>
+      [id, author, title, description, url, urlToImage, publishedAt, content];
+}
